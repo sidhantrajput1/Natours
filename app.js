02cdc36d29c4a2247fd012d77a47872d7447585e
sidhantrajput1 <sidhant808310@gmail.com>
2024-09-13 14:41:13 +0530

    add global middleware

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express();
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError.js')
+const globalErrorHandler = require('./controllers/errorControllers.js')
 const tourRouter = require('./routes/tourRoutes.js');
 const userRouter = require('./routes/userRoutes.js');
 
@@ -29,5 +31,19 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+app.all('*', (req, res, next) => {
+    // res.status(404).json({
+    //     status : 'fail',
+    //     message : `Can't Find ${req.originalUrl} on this server`
+    // })
+
+    // const err = new Error(`Can't Find ${req.originalUrl} on this server`)
+    // err.status = 'fail';
+    // err.statusCode = 404
+
+    next(new AppError(`Can't Find ${req.originalUrl} on this server`))
+})
+
+app.use(globalErrorHandler)
 
 module.exports = app;
