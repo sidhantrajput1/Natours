9887b9d1e5205a61b5b77bd350c148ec708bc9d6
sidhantrajput1 <sidhant808310@gmail.com>
2024-09-25 18:20:45 +0530

    review

--- test.js
+++ test.js
@@ -1,25 +1,27 @@
 
 
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 
 
-const cb1 = (req,res,next)=>{
-    console.log("hi from cb1")
-    next()
-}
+// const cb1 = (req,res,next)=>{
+//     console.log("hi from cb1")
+//     next()
+// }
 
-const cb2 = (req,res,next)=>{
-    console.log("hi from cb2")
-    next()
-}
+// const cb2 = (req,res,next)=>{
+//     console.log("hi from cb2")
+//     next()
+// }
 
-app.get("/",[cb1,cb2],(req,res)=>{
-    console.log("hi from request");
-    return res.send("healthy server")
-})
+// app.get("/",[cb1,cb2],(req,res)=>{
+//     console.log("hi from request");
+//     return res.send("healthy server")
+// })
 
 
-app.listen(3000, () => {
-    console.log();
-})
+// app.listen(3000, () => {
+//     console.log();
+// })
+
+// console.log("Hello from the javaScript");
